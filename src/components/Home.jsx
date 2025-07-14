3667681bf1d5c655980c81ac09f2c5b5c3b16578
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-07-14 10:59:58 +0500

    home routed in app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import light from "../assets/lightning.png";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <section>
+      <div>
+        <div>
+          <h1>
+            The Fastest Pizza <img src={light} alt="" /> Delivery
+          </h1>
+        </div>
+
+        <div></div>
+      </div>
+    </section>
+  );
 }
